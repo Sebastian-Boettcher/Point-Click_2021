90694914d24d6d2fd51bd450041d33010685c44c
Sebastian-Boettcher <84092041+Sebastian-Boettcher@users.noreply.github.com>
2021-06-24 12:51:31 +0200

    Added Sound to MainMenu

--- Point_Click_2021/Assets/Scripts/MainMenu.cs
+++ Point_Click_2021/Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEditor;
 
@@ -21,19 +22,23 @@ public class MainMenu : MonoBehaviour
     public void QuitGame ()
     {
         Debug.Log("Quit!");
+        
         Application.Quit();
     }
     //----------------------------------------------------------------------
     public void NewGame()
     {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void StartNewGame ()
     {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
     }
     public void New_Back () // Zurückknopf für die Szene NewGame
     {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
     //------------------------------------------------------------------------
---
+++ Point_Click_2021/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    } 
+
+    public void MuteMusic(){
+        mixer.SetFloat("MusicVol", -80);
+    }   
+}
