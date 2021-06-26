3ac20a24f76fc51704743c3e1215895f80307c82
Sebastian-Boettcher <84092041+Sebastian-Boettcher@users.noreply.github.com>
2021-06-26 17:54:49 +0200

    Musiksteuerung
    Lautstärke kann gespeichert werden mit PlayerPrefs

--- Point_Click_2021/Assets/Scripts/SetVolume.cs
+++ Point_Click_2021/Assets/Scripts/SetVolume.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEditor;
 
 public class SetVolume : MonoBehaviour
 {
     public AudioMixer mixer;
+    
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+    }
 
     public void SetLevel(float sliderValue)
     {
         mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+            PlayerPrefs.SetFloat("MusicVolume", sliderValue);
     } 
-
-    public void MuteMusic(){
-        mixer.SetFloat("MusicVol", -80);
-    }   
 }
